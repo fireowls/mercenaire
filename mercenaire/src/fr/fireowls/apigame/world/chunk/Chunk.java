76056450bb16fe274beea783489a524f3735ad70
BuildTools <unconfigured@null.spigotmc.org>
2019-11-23 11:59:34 +0100

    modfiy chunk class

--- mercenaire/src/fr/fireowls/apigame/world/chunk/Chunk.java
+++ mercenaire/src/fr/fireowls/apigame/world/chunk/Chunk.java
@@ -1,7 +1,6 @@
 package fr.fireowls.apigame.world.chunk;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
